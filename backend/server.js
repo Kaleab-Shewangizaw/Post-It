03bcac7f47c3beb778f86f0bdb91983e87dbba4e
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-01-27 00:21:01 +0300

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import authRoutes from './routes/auth.routes.js';
+
+const app = express()
+
+app.use("/api/auth/", authRoutes)
+
+
+app.listen(5000, () => { console.log('Server is running on port 5000') })
